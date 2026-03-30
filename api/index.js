debd85dbacb0c38b7ace8ec41d5f15f40063ccbf
Mohith <mohithrongali3@gmail.com>
2026-03-30 19:10:19 +0530

    Migrate to Zero-Config structure - move client/ to public/

--- api/index.js
+++ api/index.js
@@ -43,7 +43,7 @@ app.use('/api/health', async (req, res) => {
 });
 
 // Serve Static Frontend Files
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 // Setup database and start server
 const PORT = process.env.PORT || 3000;
